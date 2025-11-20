7f8437b2cc80cc930b80e81a7ee02f052dd9e177
nikgodda <96114399+nikgodda@users.noreply.github.com>
2025-11-20 19:57:34 +0500

    modified:   bin/bf6mod.js

--- bin/bf6mod.js
+++ bin/bf6mod.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-// Load the compiled CLI handler from dist
-// src/cli/build.ts exports:  export default async function run(args)
-import run from '../dist/cli/build.js'
+import run from "../dist/cli/build.js"
 
-// Pass command line arguments (ignoring "node" and script path)
 run(process.argv.slice(2))
